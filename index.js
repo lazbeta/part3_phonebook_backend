22007114f4eba706203fc3c7430f6e381eedfb3b
bet97 <betka.kasanicka@gmail.com>
2022-02-23 13:31:22 +0200

    adding error

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-morgan.token('body', (request) => JSON.stringify(request.body))
-app.use(express.json())
+const cors = require('cors')
+const morgan = require('morgan')
+require('dotenv').config()
 
 const Person = require('./modules/person')
 
-const cors = require('cors')
+app.use(express.json())
 app.use(cors())    
-
 app.use(express.static('build'))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+morgan.token('body', (request) => JSON.stringify(request.body))
 
 app.get('/', (request, response) => {
     response.send('<h1>hello world!</h1>')
@@ -25,53 +25,85 @@ app.get('/api/persons', (request, response) => {
 
 app.get('/info', (request, response) => {
     Person.find({}).then(persons => {
-        response.json(`Phonebook has info of ${persons.length} people. ${new Date ()}`)
+        response.send(
+            `<div>
+            <p>Phonebook has info of ${persons.length} people.</p>
+            <p>${new Date().toString()}</p>
+            </div>`
+        )
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    
-    if (person) {
-        return response.json(person)
-    } else {
-        response.status(404).end()
-    }
-})
 
-app.post ('/api/persons', (request, response) => {
+app.post ('/api/persons', (request, response, next) => {
     const body = request.body
 
     const person = new Person ({
         name: body.name,
         number: body.number,
     })
-  
-    /*  const noDuplicates = persons.find(p => p.name === body.name)
-    else if (noDuplicates) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    } */
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'you must include name and number'
-        })
-    }
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    Person.findByIdAndRemove(request.params.id).then(result=> {
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+    .then(result=> {
         response.status(204).end()
     })
-    .catch(error => next(error))
+    .catch(error => next(error)) 
     
 })
+
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => {next(error)}) 
+ 
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => {
+        response.json(updatedPerson.toJSON())
+    })
+    .catch((error) => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }  else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+    next(error)
+  }
+  app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- modules/person.js
+++ modules/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const mongoUrl = process.env.MONGODB_URI
 
@@ -11,10 +12,19 @@ mongoose.connect(mongoUrl)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-})
+    name: {
+        type: String,
+        minlength: [3, 'Must be at least 3 charachters long'],
+        required: true,
+        unique: true
+    },
+    number: {
+        type: String, 
+        minlength: [8, ' Must be at least 8 charachters long'],
+        required: true
+    }
 
+})
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
@@ -24,25 +34,6 @@ personSchema.set('toJSON', {
     }
 })
 
+personSchema.plugin(uniqueValidator)
+
 module.exports = mongoose.model('Person', personSchema)
-
-
-/*if (process.argv.length === 3){
-    Person.find({}).then(result => {
-        console.log("Phonebook:")
-        result.forEach(person => {
-        console.log(person.name, " ", person.number)
-        })
-        mongoose.connection.close()
-    })
-}
- else if(process.argv.length === 5){
-        const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4]
-        })
-        person.save().then(response => {
-        console.log(` ${process.argv[3]} was added to the phonebook`)
-        mongoose.connection.close()
-        })
-        } */
