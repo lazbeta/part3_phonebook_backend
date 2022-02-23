29192ee5a621aa63bbc49976b1f08148c437bd7f
bet97 <betka.kasanicka@gmail.com>
2022-02-23 15:58:09 +0200

    small changes

--- modules/person.js
+++ modules/person.js
@@ -36,7 +36,7 @@ const personSchema = new mongoose.Schema({
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
+        delete returnedObject._id 
         delete returnedObject.__v
     }
 })
